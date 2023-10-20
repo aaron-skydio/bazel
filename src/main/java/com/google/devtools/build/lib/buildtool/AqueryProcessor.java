ecec23b8249b50a28c5d7ce2d6d61114ea4f7696
Googler <lberki@google.com>
2023-10-20 03:56:22 -0700

    Add a new field to "bazel aquery" that reports scheduling dependencies.
    Aquery previously reported these files as being the inputs of a middleman action whose output is a regular input of the action. They are now a separate concept called "scheduling dependencies" which need to be handled separately.
    
    RELNOTES[NEW]: "bazel aquery" now returns the headers C++ compilation actions can include if the --include_scheduling_dependencies command line option is set.
    
    PiperOrigin-RevId: 575173830
    Change-Id: I4a801ec8602463afd03e45d706b4dfe1f86b11e8

--- src/main/java/com/google/devtools/build/lib/buildtool/AqueryProcessor.java
+++ src/main/java/com/google/devtools/build/lib/buildtool/AqueryProcessor.java
@@ -92,6 +92,7 @@ public final class AqueryProcessor extends PostAnalysisQueryProcessor<Configured
             new ActionGraphDump(
                 aqueryOptions.includeCommandline,
                 aqueryOptions.includeArtifacts,
+                aqueryOptions.includeSchedulingDependencies,
                 actionFilters,
                 aqueryOptions.includeParamFiles,
                 aqueryOptions.includeFileWriteContents,
